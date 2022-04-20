9cc41a219b63bfc9788d32e7ccb96fa6313d1cb5
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-20 22:06:39 +0100

    Add one more book to the database populate file

--- populate_database.py
+++ populate_database.py
@@ -10,5 +10,7 @@ author_repository.save(author1)
 author_repository.save(author2)
 
 book1 = Book("Book_1", "Hardcase", 29.99, author2)
+book2 = Book("Book_2", "Hardcase", 19.99, author2)
 
 book_repository.save(book1)
+book_repository.save(book2)

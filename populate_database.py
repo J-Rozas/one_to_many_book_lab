bb84b118c22a4246f821c0df760291036240857d
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-20 20:43:46 +0100

    Add file to populate the database for initial test

---
+++ populate_database.py
@@ -0,0 +1,14 @@
+from models.book import Book
+from models.author import Author
+
+from repositories import author_repository, book_repository
+
+author1 = Author("Author_1")
+author2 = Author("Author_2")
+
+author_repository.save(author1)
+author_repository.save(author2)
+
+book1 = Book("Book_1", "Hardcase", 29.99, author2)
+
+book_repository.save(book1)

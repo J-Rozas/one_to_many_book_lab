4982f1667fe5c985bda7c5fc16afd1b7cd6a3c7f
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-04-20 19:37:23 +0100

    Modify basic route to show all the books

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,15 +1,11 @@
-from flask import Flask, render_template, Blueprint
-from models.book import Book
+from flask import render_template, Blueprint
 from repositories import book_repository
 
 
 books_blueprint = Blueprint("books",__name__)
 
-@books_blueprint.route('/')
-def home():
-    return "Hello"
-
-@books_blueprint.route('/allbooks')
+@books_blueprint.route('/', methods=["GET"])
+@books_blueprint.route('/books', methods=["GET"])
 def books():
     book_list = book_repository.select_all()
     return render_template('all_books.html', books = book_list)
